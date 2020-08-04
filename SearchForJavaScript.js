4b59196a1f6dd0c87f3bc3abe38e96cf3f8aa73b
rezaAzimi1 <48802239+rezaAzimi1@users.noreply.github.com>
2020-08-04 19:56:14 +0430

    add search.js

---
+++ SearchForJavaScript.js
@@ -0,0 +1,32 @@
+<script>
+    var selectedElm
+    function ReplaceListContent_SelectText(elm) {
+        selectedElm.value = elm.innerText.split(":")[0]
+        selectedElm.nextElementSibling.innerHTML = ""
+    }
+    function ReplaceListContent(elm) {
+        selectedElm = elm
+        elm.addEventListener('blur', function () {
+            setTimeout(() => { elm.nextElementSibling.innerHTML = "" }, 300)
+        })
+
+        var width = elm.getBoundingClientRect().width
+        if (elm.value != "") {
+            $.ajax({
+                url: "./Filter/",
+                type: "GET",
+                data: { patern: elm.value },
+                success: (res) => {
+                    //console.log(res)
+                    var temp = ``
+                    res.forEach(element => {
+                        temp += `<li class="list-group-item" style="width:${width}px" onclick="ReplaceListContent_SelectText(this)">${element.code}:${element.name}</li>`
+                    })
+
+                    elm.nextElementSibling.innerHTML = temp
+                }
+            })
+        } else elm.nextElementSibling.innerHTML = ""
+    }
+
+</script>
