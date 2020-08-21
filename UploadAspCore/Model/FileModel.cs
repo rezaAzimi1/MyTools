d54251e181e1ff8c85aff6b49a2c4ba7faafd1a6
rezaAzimi1 <48802239+rezaAzimi1@users.noreply.github.com>
2020-08-21 19:18:52 +0430

    Create FileModel.cs

---
+++ UploadAspCore/Model/FileModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadAspCore.Models
+{
+    public class FileModel
+    {
+        public string FileContent { get; set; }
+        public string FileName  { get; set; }
+
+    }
+}
